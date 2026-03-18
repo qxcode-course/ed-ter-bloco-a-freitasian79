563330ed7872d44af4bafe586692643ae8962c93
freitasian79 <ffsilvaian0906@gmail.com>
2026-03-17 22:03:33 -0300

    atividade revisao ed

--- ed/casais/go/draft.go
+++ ed/casais/go/draft.go
@@ -1,5 +1,29 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var n int
+	fmt.Scan(&n)
+
+	animais := make([]int, n)
+
+	var totalCasais int
+	for i := 0; i < n; i++ {
+		fmt.Scan(&animais[i])
+	}
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			if animais[i] > 0 || animais[i] < 0 {
+				if animais[i] == animais[j]*-1 {
+					animais[j], animais[i] = 0, 0
+					totalCasais++
+				}
+			}
+		}
+	}
+
+	fmt.Println(totalCasais)
+
 }
--- ed/gomos/go/draft.go
+++ ed/gomos/go/draft.go
@@ -1,5 +1,41 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("qxcode")
+	var Q int
+	var d string
+
+	fmt.Scan(&Q)
+
+	fmt.Scan(&d)
+
+	x := make([]int, Q)
+	y := make([]int, Q)
+
+	// contagem dos gomos
+	for i := 0; i < Q; i++ {
+		fmt.Scan(&x[i], &y[i])
+	}
+
+	for i := Q - 1; i > 0; i-- {
+		x[i], y[i] = x[i-1], y[i-1]
+	}
+
+	//movimento da cabeça
+
+	switch d {
+	case "L":
+		x[0]--
+	case "R":
+		x[0]++
+	case "U":
+		y[0]--
+	case "D":
+		y[0]++
+	}
+
+	for i := 0; i < Q; i++ {
+		fmt.Printf("%d %d\n", x[i], y[i])
+	}
 }
